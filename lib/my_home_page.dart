c84141a09b077998b0108721c73b6be3df7fe3a5
alearal12 <alejandro.araya@ucrso.info>
2022-06-13 12:03:38 -0600

    add project

---
+++ lib/my_home_page.dart
@@ -0,0 +1,122 @@
+import 'package:crud_en_flutter/message_response.dart';
+import 'package:crud_en_flutter/edit_contact.dart';
+import 'package:crud_en_flutter/add_contact.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  final String _title;
+  MyHomePage(this._title);
+  @override
+  State<StatefulWidget> createState() => _MyHomePage();
+}
+
+class _MyHomePage extends State<MyHomePage> {
+  List<Client> clients = [
+    Client(name: 'Ale', surname: 'Araya', phone: '888888'),
+    Client(name: 'Paulo', surname: 'Correa', phone: '999999'),
+
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget._title),
+      ),
+      body: ListView.builder(
+        itemCount: clients.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            onTap: () {
+              Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (_) => EditContact(clients[index])))
+                  .then((newContact) {
+                if (newContact != null) {
+                  setState(() {
+                    clients.removeAt(index);
+
+                    clients.insert(index, newContact);
+
+                    messageResponse(
+                        context, newContact.name + " has been modified");
+                  });
+                }
+              });
+            },
+            onLongPress: () {
+              removeClient(context, clients[index]);
+            },
+            title: Text(clients[index].name + " " + clients[index].surname),
+            subtitle: Text(clients[index].phone),
+            leading: CircleAvatar(
+              child: Text(clients[index].name.substring(0, 1)),
+            ),
+            trailing: Icon(
+              Icons.call,
+              color: Colors.red,
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+                  context, MaterialPageRoute(builder: (_) => AddContact()))
+              .then((newContact) {
+            if (newContact != null) {
+              setState(() {
+                clients.add(newContact);
+                messageResponse(
+                    context, newContact.name + " has been saved");
+              });
+            }
+          });
+        },
+        tooltip: "Add Contact",
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  removeClient(BuildContext context, Client client) {
+    showDialog(
+        context: context,
+        builder: (_) => AlertDialog(
+              title: Text("Delete Client"),
+              content: Text("Do you want to delete " + client.name + "?"),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      this.clients.remove(client);
+                      Navigator.pop(context);
+                    });
+                  },
+                  child: Text(
+                    "Delete",
+                    style: TextStyle(color: Colors.red),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text(
+                    "Cancel",
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                )
+              ],
+            ));
+  }
+}
+
+class Client {
+  var name;
+  var surname;
+  var phone;
+
+  Client({this.name, this.surname, this.phone});
+}
